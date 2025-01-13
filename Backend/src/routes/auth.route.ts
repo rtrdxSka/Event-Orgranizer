553dee26bb035a34ef8a5903dcbb4e639fe95d82
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-13 16:53:25 +0200

    feat: add authentication routes with registration endpoint

---
+++ Backend/src/routes/auth.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { registerHandler } from "../controllers/auth.controller";
+
+
+const authRoutes = Router();
+
+//prefix: /auth
+
+authRoutes.post("/register", registerHandler);
+
+export default authRoutes;
