191eb40e2c82ed4f4483501623b40234c9d27c7c
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-16 18:02:33 +0200

    feat: add verifyToken function for JWT verification with error handling

--- Backend/src/utils/jwt.ts
+++ Backend/src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import { SignOptions } from "jsonwebtoken";
+import { SignOptions, VerifyOptions } from "jsonwebtoken";
 import { SessionDocument } from "../models/session.model";
 import { UserDocument } from "../models/user.model";
 import { JWT_SECRET } from "../constants/env";
@@ -39,4 +39,19 @@ export const signToken = (
   const {secret, ...signOpts} = options || accessTokenSignOptions;
 
   return jwt.sign(payload, secret, {...defaults,...signOpts});
+}
+
+export const verifyToken = <TPayload extends object = AccessTokenPayload>(token: string, options?: VerifyOptions & {secret: string}) => {
+  const {secret = JWT_SECRET, ...verifyOpts} = options || {};
+  try{
+    const payload = jwt.verify(token,secret, {...defaults, ...verifyOpts}) as TPayload;
+    return {
+      payload
+    }
+  }catch(err: any){
+    return {
+      error: err.message
+    }
+  }
+  
 }
