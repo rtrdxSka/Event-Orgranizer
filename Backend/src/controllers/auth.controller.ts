95178ab5fa9f7707151e897dd1e2b628b62384b8
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-16 17:24:29 +0200

    feat: implement login handler with schema validation and cookie setting

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -1,20 +1,10 @@
-import { CREATED } from "../constants/http";
-import { createAccount } from "../services/auth.service";
+import { CREATED, OK } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
 import catchErrors from "../utils/catchErrors";
-import {z} from "zod";
 import { setAuthCookies } from "../utils/cookies";
+import { loginSchema, registerSchema } from "./auth.schemas";
 
-const registerSchema = z.object({
-  email: z.string().email().min(1).max(255),
-  password: z.string().min(6).max(255),
-  confirmPassword: z.string().min(6).max(255),
-  userAgent: z.string().optional(),
-}).refine(
-  (data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  }
-)
+
 
 
 export const registerHandler = catchErrors(
@@ -31,4 +21,14 @@ export const registerHandler = catchErrors(
     //return response
     return setAuthCookies({res, accessToken, refreshToken}).status(CREATED).json(user);
   }
-)
+);
+
+export const loginHandler = catchErrors( async (req,res) => {
+  const request = loginSchema.parse({...req.body, userAgent: req.headers["user-agent"]});
+
+  const {accessToken, refreshToken} = await loginUser(request);
+
+  return setAuthCookies({res, accessToken, refreshToken}).status(OK).json({
+    message: "Login successful"
+  });
+})
