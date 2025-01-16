19d146f6de36c4e459a4576a0b78681f496bfa92
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-16 17:24:40 +0200

    feat: enhance error handling with AppError support in middleware

--- Backend/src/middleware/errorHandler.ts
+++ Backend/src/middleware/errorHandler.ts
@@ -2,6 +2,7 @@ import { ErrorRequestHandler } from "express";
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
 import {z} from "zod";
 import { Response } from "express";
+import AppError from "../utils/AppError";
 
 
 const handleZodError = (res: Response, error: z.ZodError) => {
@@ -16,12 +17,24 @@ const handleZodError = (res: Response, error: z.ZodError) => {
   })
 }
 
+const handleAppError = (res: Response, error: AppError) => {
+    res.status(error.statusCode).json({
+    message: error.message,
+    errorCode: error.errorCode
+  });
+}
+
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.log(`PATH: ${req.path}`, error);
 
   if(error instanceof z.ZodError) {
    handleZodError(res, error);
    return;
+  }
+
+  if (error instanceof AppError) {
+     handleAppError(res, error);
+     return;
   }
 
  res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
