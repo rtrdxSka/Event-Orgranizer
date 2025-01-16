918da397973e6aa0d58ab448b0fa6903affb32d9
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-16 18:02:19 +0200

    feat: add logout and refresh routes to authentication API

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, registerHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshHandler, registerHandler } from "../controllers/auth.controller";
 
 
 const authRoutes = Router();
@@ -8,5 +8,7 @@ const authRoutes = Router();
 
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
+authRoutes.get("/refresh", refreshHandler);
+authRoutes.get("/logout", logoutHandler);
 
 export default authRoutes;
