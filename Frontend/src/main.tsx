63c743fbc088525febc831b2726d6affc1b3ca9d
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-21 16:10:59 +0200

    feat: import global CSS styles in main entry point

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClientProvider } from '@tanstack/react-query'
 import queryClient from './config/queryClient.ts'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
