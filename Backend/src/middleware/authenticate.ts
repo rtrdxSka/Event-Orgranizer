fcf98c57742f64dbabc8a39b1479b24d1b68b97a
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-21 15:42:28 +0200

    feat: implement authentication middleware to verify access tokens and attach user/session IDs

---
+++ Backend/src/middleware/authenticate.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from "express";
+import appAssert from "../utils/appAssert";
+import AppErrorCode from "../constants/appErrorCode";
+import { AccessTokenPayload, verifyToken } from "../utils/jwt";
+import { UNAUTHORIZED } from "../constants/http";
+import mongoose from "mongoose";
+
+
+const authenticate:RequestHandler = (req, res, next) => {
+  const accessToken = req.cookies.accessToken as string | undefined;
+
+  appAssert(accessToken, 401, "Unauthorized", AppErrorCode.InvalidAccessToken);
+
+  const {error, payload} = verifyToken<AccessTokenPayload>(accessToken);
+  appAssert(payload,UNAUTHORIZED, error === "jwt expired" ? "Token expired" : "Invalid Token", AppErrorCode.InvalidAccessToken);
+
+  req.userId = payload.userId as mongoose.Types.ObjectId;
+  req.sessionId = payload.sessionId as mongoose.Types.ObjectId;
+  next();
+};
