c4c173f05c86e54328dc368275b8e762c061b360
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-21 15:43:31 +0200

    initial commit frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+
+export const Home = ()  => {
+    return (
+        <div>Home</div>
+    );
+}
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
