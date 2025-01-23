00122c9a8438346b4ec69e5f3a859f3b834641f8
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-01-23 17:32:54 +0200

    feat: add routes for Register, VerifyEmail, ForgotPassword, and ResetPassword pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Register from "./pages/Register"
+import VerifyEmail from "./pages/VerifyEmail"
+import ForgotPassword from "./pages/ForgotPassword"
+import ResetPassword from "./pages/ResetPassword"
 
 function App() {
   
@@ -9,6 +13,10 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/email/verify/:code" element={<VerifyEmail />} />
+      <Route path="/password/forgot" element={<ForgotPassword />} />
+      <Route path="/password/reset" element={<ResetPassword />} />
     </Routes>
   )
 }
