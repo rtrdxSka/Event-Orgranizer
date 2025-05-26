ff72aa71b92b569ccd7cce66d5a96a002c5bf672
Kristiqn Mitev <116177967+rtrdxSka@users.noreply.github.com>
2025-05-26 21:27:28 +0300

    feat: add googleTokens field to UserDocument and userSchema for Google authentication

--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -8,6 +8,13 @@ export interface UserDocument extends mongoose.Document {
   verified: boolean;
   createdAt: Date;
   updatedAt: Date;
+  googleTokens?: {
+    access_token: string;
+    refresh_token?: string;
+    expiry_date?: number;
+    token_type?: string;
+    scope?: string;
+  };
   comparePassword(val: string) :Promise <boolean>;
   omitPassword(): Pick<UserDocument, "_id" | "email" | "verified" | "createdAt" | "updatedAt">;
 }
@@ -17,6 +24,16 @@ const userSchema = new mongoose.Schema<UserDocument>({
   email: {type: String, unique: true, required: true},
   password: {type: String, required: true},
   verified: {type: Boolean, default: false, required: true},
+  googleTokens: {
+    type: {
+      access_token: { type: String, required: true },
+      refresh_token: { type: String },
+      expiry_date: { type: Number },
+      token_type: { type: String },
+      scope: { type: String }
+    },
+    required: false
+  }
 }, {
   timestamps: true,
 })
